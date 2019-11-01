e479fe00aeca78ec750afc62dbfb881c887fce0c
pa1007 <pa.fourriere@pa1007.fr>
2019-11-01 15:54:25 +0100

    Change frame dimension

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("/view/TypePlatView.fxml"));
         primaryStage.setTitle("TypePlat");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 600, 275));
         primaryStage.show();
     }
 
