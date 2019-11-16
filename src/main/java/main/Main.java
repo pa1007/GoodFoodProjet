ea3324a4353d144610a9a819fd9462a3b6244e35
pa1007 <pa.fourriere@sfr.fr>
2019-11-16 23:09:56 +0100

    PL SQL

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("/view/TypePlatView.fxml"));
         primaryStage.setTitle("TypePlat");
-        primaryStage.setScene(new Scene(root, 1200, 275));
+        primaryStage.setScene(new Scene(root, 1200, 500));
         primaryStage.show();
     }
 
